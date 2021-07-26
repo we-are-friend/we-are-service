e9a6e72b22f6a2b99f73b66f2832fd4a633f1190
kitravee <kitravee.swk@gmail.com>
2021-07-27 02:20:03 +0700

    Delete - unuse

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TaskModule],
 })
 export class AppModule {}
