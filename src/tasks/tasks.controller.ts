8e1d9adbf75c217f207d8c7fa62ecea4ade8c633
kitravee <kitravee.swk@gmail.com>
2021-07-28 02:55:23 +0700

    Defining a Task Model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -10,9 +11,10 @@ export class TasksController {
 
   //handler method
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
   // helloWorld() {
   //   this.taskService;
   // }
