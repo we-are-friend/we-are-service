7cdb8f5930475ae35fca2586ea2078101e653396
kitravee <kitravee.swk@gmail.com>
2021-07-31 20:12:54 +0700

    Dto Data tranfer object

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -13,6 +14,27 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // @Post()
+  // createTask(@Body() body) {
+  //   console.log(
+  //     '🚀 ~ file: tasks.controller.ts ~ line 21 ~ TasksController ~ body',
+  //     body,
+  //   );
+  // }
+
+  // @Post()
+  // createTask(
+  //   @Body('title') title: string,
+  //   @Body('description') description: string,
+  // ): Task {
+  //   return this.tasksService.createTask(title, description);
+  // }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 
   // helloWorld() {
